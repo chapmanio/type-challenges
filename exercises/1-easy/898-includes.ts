8c712666a54faa836b98d1f96e433338338ad705
Richard Chapman <rcchapman85@gmail.com>
2022-06-09 10:20:10 +0100

    898-includes solution

--- exercises/1-easy/898-includes.ts
+++ exercises/1-easy/898-includes.ts
@@ -18,35 +18,45 @@
 
 /* _____________ Your Code Here _____________ */
 
+/*
+  NOTES
+  - Try and get the first item in the array, store this and the remaining items using `infer`
+  - Check if the first item matches the supplied key
+  - Otherwise, check the remaining items match the supplied key (calling same type function)
+  - Code behind `Equal` shown - converting types to a boolean before comparing
+*/
+
+export type MyEqual<TypeOne, TypeTwo> = (<Type>() => Type extends TypeOne ? 1 : 2) extends <
+  Type
+>() => Type extends TypeTwo ? 1 : 2
+  ? true
+  : false;
+
 type Includes<Array extends readonly any[], Key> = Array extends [
   infer FirstItem,
   ...infer RestOfItems
 ]
-  ? Equal<FirstItem, Key> extends true
+  ? MyEqual<FirstItem, Key> extends true
     ? true
     : Includes<RestOfItems, Key>
   : false;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from "@type-challenges/utils";
+import type { Equal, Expect } from '@type-challenges/utils';
 
 type cases = [
-  Expect<
-    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">, true>
-  >,
-  Expect<
-    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">, false>
-  >,
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
   Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
   Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
   Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
   Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
-  Expect<Equal<Includes<[{}], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
   Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
   Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
   Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
-  Expect<Equal<Includes<[{ a: "A" }], { readonly a: "A" }>, false>>,
-  Expect<Equal<Includes<[{ readonly a: "A" }], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
   Expect<Equal<Includes<[1], 1 | 2>, false>>,
   Expect<Equal<Includes<[1 | 2], 1>, false>>,
   Expect<Equal<Includes<[null], undefined>, false>>,
